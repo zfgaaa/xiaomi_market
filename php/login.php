357e5b3ab53d3650f8888db0252ceea99c6dbc7d
zfgaaa <1248639502@qq.com>
2018-10-10 14:07:34 +0800

    登录

--- php/login.php
+++ php/login.php
@@ -3,8 +3,23 @@
     header("Content-type: application/json");
     header("Access-Control-Allow-Origin:*");
     
-    $json = file_get_contents("php://input");
+    include "public/connect_db.php";
+
+    $json = json_decode(file_get_contents("php://input"));
     $username = $json -> username;
-    var_dump($username);
+    $password = $json -> password;
+
+
+    $coon = new db();
+    $sql = "SELECT * FROM user_table WHERE username='$username' and password='$password'";
+    $rows = $coon -> Query($sql, 2);
+    if($rows){
+        //用户名输入正确
+        $arr = array("code" => "200", "msg"=>"", "data"=>array("id"=>$rows["id"], "token"=>"1234567899", "atavar"=> "http://www.aaa.com/path/a.png"));
+    } else {
+      // 输入错误
+      $arr = array("code" => "1000", "msg" => "用户名或密码输入错误");
+    }
+    echo json_encode($arr);
 
 ?>
